a48029c5c838c7929dd0d54a5011b418051a72dd
samson <samson.c.wang@gmail.com>
2019-07-11 00:02:12 +0800

    A working face find and match demo

--- deploy/test.py
+++ deploy/test.py
@@ -26,16 +26,43 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     model = face_model.FaceModel(args)
-    img = cv2.imread(args.image)
-    imgs, bbox, points = model.get_input(img)
-    #feas = np.array([model.get_feature(img) for img in imgs])
-    feas = model.get_feature(imgs)
-    
-    print(feas.shape)
-    print(np.sum(np.square(feas[0] - feas[1])), np.dot(feas[0], feas[1]))
-    #gender, age = model.get_ga(img)
-    #print(gender)
-    #print(age)
+    total_feas = []
+    face_images = []
+    for image in args.image:
+        img = cv2.imread(image)
+        max_size = max(img.shape)
+        scale = 768. / max_size
+        img = cv2.resize(img, None, fx=scale, fy=scale)
+        imgs, bbox, points = model.get_input(img)
+        face_images.append(imgs)
+        #feas = np.array([model.get_feature(img) for img in imgs])
+        feas = model.get_feature(imgs)
+        
+        print(feas.shape)
+        print(bbox)
+#        print(np.sum(np.square(feas[0] - feas[1])))
+        print(np.dot(feas, feas.T))
+        for b in bbox:
+            bb = b.astype(np.int).tolist()
+            cv2.rectangle(img, (bb[0], bb[1]), (bb[2], bb[3]), 255, 2)
+        #cv2.imshow("bbox", img)
+        #cv2.waitKey()
+        total_feas.append(feas)
+    total_feas = np.concatenate(total_feas, axis=0)
+    face_images = np.concatenate(face_images, axis=0)
+    dist = np.dot(total_feas, total_feas.T) * (1. - np.eye(total_feas.shape[0]))
+    print(np.where(dist > 0.5))
+    for x,y in np.array(np.where(dist > 0.5)).T:
+        print(x,y)
+        if x < y:
+            continue
+        cv2.imshow("l", face_images[x].transpose((1,2,0)))
+        cv2.imshow("r", face_images[y].transpose((1,2,0)))
+        print(dist[x,y])
+        cv2.waitKey()
+        #gender, age = model.get_ga(img)
+        #print(gender)
+        #print(age)
     sys.exit(0)
     img = cv2.imread('/raid5data/dplearn/megaface/facescrubr/112x112/Tom_Hanks/Tom_Hanks_54733.png')
     f2 = model.get_feature(img)
