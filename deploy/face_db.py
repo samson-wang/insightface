ed1aff837abb54898b2c0997cbc656125b9251a7
samson wang <samson.c.wang@gmail.com>
2019-07-15 20:09:11 +0800

    Sync

---
+++ deploy/face_db.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+
+class FaceDB(object):
+    '''
+    Face DataBase.
+    To store, face id / feature vector, image list, face box and points.
+    '''
+    def __init__(self, db_path):
+        '''
+        Initialization
+        '''
+
+        self.
