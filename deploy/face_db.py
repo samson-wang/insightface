4aef9945a56cea565fa6659ce1d3f190fa1d4786
samson <samson.c.wang@gmail.com>
2019-07-25 07:41:35 +0800

    for sync

--- deploy/face_db.py
+++ deploy/face_db.py
@@ -10,5 +10,21 @@ class FaceDB(object):
         '''
         Initialization
         '''
-
-        self.
+        # person's feature, (P x (N+1))
+        self.fea_vector = None
+        # Inverse map bbox to person and image, (B x (1 + 1))
+        self.inv_map = None
+        # Avatar image of bbox, (B x 64 x 64 x 3)
+        self.avatar = None
+        # Image list, (I x 1)
+        self.image_list = None
+       
+    def check(self, image_info, boxes, points, feas):
+        '''
+        To check and update facedb.
+        '''
+        if self.fea_vector is None:
+            self.fea_vector = feas.copy()
+            self.image_list = [image_info['fn']]
+            self.inv_map = dict(zip(range(feas.shape[0]), range(feas.shape[0])))
+        
